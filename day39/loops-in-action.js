7b6b35fb2ae0b369c981146f875deb0b387ad86c
sen <mse931215@gmail.com>
2023-05-11 00:50:23 +0900

    day39+

--- day39/loops-in-action.js
+++ day39/loops-in-action.js
@@ -1,4 +1,4 @@
-// First Example: Sum numbers
+//for / First Example: Sum numbers
 
 const calculateSumButtonElement = document.querySelector("#calculator button");
 
@@ -20,7 +20,7 @@ function calculateSum() {
 
 calculateSumButtonElement.addEventListener("click", calculateSum);
 
-//Highlight links
+//for of / Highlight links
 
 const highlightLinksButtonElement = document.querySelector(
   "#highlight-links button"
@@ -35,3 +35,70 @@ function highlightLinks() {
 }
 
 highlightLinksButtonElement.addEventListener("click", highlightLinks);
+
+//for in / 사용자 데이터 보여주기 -> 나중에 다시보기
+
+const dummyUserData = {
+  firstName: "세은",
+  lastName: "문",
+  age: 29,
+};
+
+const displayUserDataButtonElement =
+  document.querySelector("#user-data button");
+
+function displayUserData() {
+  const outputDataElement = document.getElementById("output-user-data");
+
+  outputDataElement.innerHTML = "";
+
+  for (const key in dummyUserData) {
+    const newUserDataListItemElement = document.createElement("li");
+    const outputText = key.toUpperCase() + ": " + dummyUserData[key];
+    newUserDataListItemElement.textContent = outputText;
+    outputDataElement.append(newUserDataListItemElement);
+  }
+}
+
+displayUserDataButtonElement.addEventListener("click", displayUserData);
+
+//while / 통계 주사위 굴리기 receive king
+
+const rollDiceButtonElement = document.querySelector("#statistics button");
+
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1; // Math.random(): 0~1 사이 랜덤 숫자 반환. Math.floor(): 반내림.
+}
+function deriveNumberOfDiceRolls() {
+  const targetNumberInputElement =
+    document.getElementById("user-target-number");
+  const diceRollsListElement = document.getElementById("dice-rolls");
+
+  const enteredNumber = targetNumberInputElement.value;
+  diceRollsListElement.innerHTML = "";
+
+  let hasRolledTargetNumber = false;
+  let numberOfRolls = 0;
+
+  while (!hasRolledTargetNumber) {
+    const rolledNumber = rollDice();
+    // if(rolledNumber == enteredNumber) { //입력된 숫자도 문자열 취급이므로 === 아니라 == 사용
+    // hasRolledTargetNumber = true;
+    // } 이해가 쉬운 1번 방법.  아래는 2번 방법
+    numberOfRolls++;
+    const newRollListElement = document.createElement("li");
+    const outputText = "Roll " + numberOfRolls + ": " + rolledNumber;
+    newRollListElement.textContent = outputText;
+    diceRollsListElement.append(newRollListElement);
+    hasRolledTargetNumber = rolledNumber == enteredNumber;
+  }
+
+  const outputTotalRollsElement = document.getElementById("output-total-rolls");
+  const outputTargetNumberElement = document.getElementById(
+    "output-target-number"
+  );
+  outputTargetNumberElement.textContent = enteredNumber;
+  outputTotalRollsElement.textContent = numberOfRolls;
+}
+
+rollDiceButtonElement.addEventListener("click", deriveNumberOfDiceRolls);
