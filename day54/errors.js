dba8b89c1a82e2bd0bcda0a0a1acfb4eec71cf96
sen <mse931215@gmail.com>
2023-06-02 23:58:57 +0900

    day55_01

---
+++ day54/errors.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+function readFile() {
+  let fileData;
+  try {
+    const fileData = fs.readFileSync("data.json");
+  } catch {
+    console.log("에러 발생");
+  }
+}
+console.log(fileData);
+console.log("안녕");
+
+readFile();
