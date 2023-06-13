22d4bf349c71db37bd371a5f966b1f0d802fed74
sen <mse931215@gmail.com>
2023-06-13 23:40:36 +0900

    day61_password

--- day60~/data/database.js
+++ day60~/data/database.js
@@ -1,10 +1,11 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: "localhost",
   database: "blog",
   user: "root",
-  password: "<your-pw>",
+  password: "비밀번호",
 });
 // 연결 풀 생성.
+//비밀번호 확인 혹은 변경 필요 ★
 module.exports = pool;
