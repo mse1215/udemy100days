a5d16fa3c177d5038b158882ac8f1322665a3c88
sen <mse931215@gmail.com>
2023-06-14 23:58:51 +0900

    day61_01

--- day60~/data/database.js
+++ day60~/data/database.js
@@ -1,10 +1,10 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: "localhost",
   database: "blog",
   user: "root",
-  password: "비밀번호",
+  password: "pw",
 });
 // 연결 풀 생성.
 //비밀번호 확인 혹은 변경 필요 ★
--- day60~/routes/blog.js
+++ day60~/routes/blog.js
@@ -17,4 +17,19 @@ router.get("/new-post", async function (req, res) {
   res.render("create-post", { authors: authors });
 });
 
+router.post("/posts", async function (req, res) {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+  //name을 기준으로
+  await db.query(
+    "INSERT INTO posts (title, summary, body, authors_id) VALUES (?)",
+    [data[0], data[1], data[2], data[3]]
+  );
+  res.redirect("/posts");
+});
+
 module.exports = router;
