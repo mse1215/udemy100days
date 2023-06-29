20e26f0b354afabbe858598abac681eb546b68bc
sen <mse931215@gmail.com>
2023-06-29 23:18:55 +0900

    day67_03

--- day67~/routes/users.js
+++ day67~/routes/users.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const multer = require("multer");
 
-const upload = multer({});
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images"); //multer는 세번째 매개변수 값에 관한 함수를 제공. 여기서는 이 함수에 관해 두 가지 값을 기대함. 첫번째: 잠재적인 오류, 두번쨰: 파일 지정 경로
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+// const upload = multer({ dest: "Images" }); //images 폴더에 저장
+const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
 router.get("/", function (req, res) {
@@ -14,8 +24,13 @@ router.get("/new-user", function (req, res) {
 
 router.post("/profiles", upload.single("image"), function (req, res) {
   //모든 요청에 적용되지 않음. 일부 요청(양식 제출-파일 첨부)을 처리하는 router
-  const uploadedImageFile = req.file(); //요청에 연결된 전체 데이터에 액세스 할 수 있음
+  const uploadedImageFile = req.file; //요청에 연결된 전체 데이터에 액세스 할 수 있음
   const userData = req.body; //name: username
+
+  console.log(uploadedImageFile);
+  console.log(userData);
+
+  res.redirect("/");
 });
 
 module.exports = router;
