963663705e86a5d0c505327ebf71cd09a13d6f97
sen <mse931215@gmail.com>
2023-06-30 23:57:20 +0900

    day68_01

--- day67~/data/database.js
+++ day67~/data/database.js
@@ -1,17 +1,17 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function connectToDatabase() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('file-demo');
+  const client = await MongoClient.connect("mongodb://localhost:27017");
+  database = client.db("file-demo"); // 아직 존재하지 않는 경우엔 자동으로 생성.
 }
 
 function getDb() {
   if (!database) {
-    throw { message: 'Database not connected!' };
+    throw { message: "Database not connected!" };
   }
   return database;
 }
--- day67~/routes/users.js
+++ day67~/routes/users.js
@@ -1,34 +1,37 @@
 const express = require("express");
 const multer = require("multer");
 
+const db = require("../data/database");
+
 const storageConfig = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "images"); //multer는 세번째 매개변수 값에 관한 함수를 제공. 여기서는 이 함수에 관해 두 가지 값을 기대함. 첫번째: 잠재적인 오류, 두번쨰: 파일 지정 경로
+    cb(null, "images");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-// const upload = multer({ dest: "Images" }); //images 폴더에 저장
 const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.render("profiles");
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection("users").find().toArray();
+  res.render("profiles", { users: users });
 });
 
 router.get("/new-user", function (req, res) {
   res.render("new-user");
 });
 
-router.post("/profiles", upload.single("image"), function (req, res) {
-  //모든 요청에 적용되지 않음. 일부 요청(양식 제출-파일 첨부)을 처리하는 router
-  const uploadedImageFile = req.file; //요청에 연결된 전체 데이터에 액세스 할 수 있음
-  const userData = req.body; //name: username
+router.post("/profiles", upload.single("image"), async function (req, res) {
+  const uploadedImageFile = req.file;
+  const userData = req.body;
 
-  console.log(uploadedImageFile);
-  console.log(userData);
+  await db.getDb().collection("users").insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path,
+  });
 
   res.redirect("/");
 });
