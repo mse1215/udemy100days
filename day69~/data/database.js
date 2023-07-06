2c7915f42a18a3f950032a3704b9146eb895b2a0
sen <mse931215@gmail.com>
2023-07-06 22:40:53 +0900

    day69_00

--- day69~/data/database.js
+++ day69~/data/database.js
@@ -1,22 +1,22 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function connect() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('blog');
+  const client = await MongoClient.connect("mongodb://localhost:27017");
+  database = client.db("blog");
 }
 
 function getDb() {
   if (!database) {
-    throw { message: 'Database connection not established!' };
+    throw { message: "데이터베이스 연결이 이상해요!" };
   }
   return database;
 }
 
 module.exports = {
   connectToDatabase: connect,
-  getDb: getDb
+  getDb: getDb,
 };
