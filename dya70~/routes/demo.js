361f79c759b5fa5e678ba3dc278c54279f867842
sen <mse931215@gmail.com>
2023-07-09 23:50:25 +0900

    day71_01

--- dya70~/routes/demo.js
+++ dya70~/routes/demo.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const db = require("../data/database");
 
@@ -21,9 +22,10 @@ router.post("/signup", async function (req, res) {
   const enteredEmail = userData.email; // = userData["email"]
   const enteredConfirmEmail = userData["confirm-email"]; // '-'를 사용할 수 없으므로 대체 표기법 [] 사용
   const enteredPassword = userData.password;
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12); //12 -> 해싱강도
   const user = {
     email: enteredEmail,
-    password: enteredPassword,
+    password: hashedPassword,
   };
 
   await db.getDb().collection("users").insertOne(user);
@@ -31,7 +33,34 @@ router.post("/signup", async function (req, res) {
   res.redirect("/login");
 });
 
-router.post("/login", async function (req, res) {});
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (!existingUser) {
+    console.log("로그인을 할 수 없습니다!");
+    return res.redirect("/login");
+  }
+
+  const passwordsAreEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+
+  if (!passwordsAreEqual) {
+    console.log("로그인을 할 수 없습니다! - 비밀번호가 일치하지 않습니다!");
+    return res.redirect("/login");
+  }
+
+  console.log("사용자 인증 완료");
+  res.redirect("/admin");
+});
 
 router.get("/admin", function (req, res) {
   res.render("admin");
